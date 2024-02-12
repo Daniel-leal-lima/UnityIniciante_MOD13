c27cfa2d56aefc727781aa6e0b21684e7800d414
Daniel-leal-lima <daniel.leal.lima@hotmail.com>
2024-02-12 11:52:20 -0300

    crate numbers working

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Crate : MonoBehaviour, IHitable
 {
     int numberOfHitsToDestroy;
     [SerializeField] GameObject fruitPrefab;
+    [SerializeField] TextMeshPro[] text;
+    private Rigidbody rb;
 
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         numberOfHitsToDestroy = Random.Range(1, 4);
+        UpdateText();
     }
     public void TakeHit()
     {
-        if (numberOfHitsToDestroy > 0)
+        if (numberOfHitsToDestroy > 1)
         {
             numberOfHitsToDestroy--;
+            UpdateText();
         }
-        else if (numberOfHitsToDestroy == 0)
+        else
         {
             GameObject fruit = Instantiate(fruitPrefab, transform.position, transform.localRotation ,Spawner.spawnerContainer);
             if (fruit.gameObject.TryGetComponent(out Rigidbody rb))
@@ -25,6 +31,13 @@ public class Crate : MonoBehaviour, IHitable
                 rb.AddForce(Vector3.up * 2, ForceMode.Impulse);
             }
             Destroy(gameObject);
+        }
+    }
+    void UpdateText()
+    {
+        foreach (var item in text)
+        {
+            item.text = numberOfHitsToDestroy.ToString();
         }
     }
 }
