3a1340b0e8835ce8b33aa73a1d9c93b283a63061
Daniel-leal-lima <daniel.leal.lima@hotmail.com>
2024-02-13 16:50:59 -0300

    switching mobile button to touch for mobile

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -6,44 +6,12 @@ public class Pointer : MonoBehaviour
 {
     [SerializeField] LayerMask layer;
     [SerializeField] GameObject EffectGO;
-    [SerializeField] Camera mainCamera;
     Vector3 oldPos;
     Vector3 direction;
     private void Update()
     {
-        //TODO Works with touch
-        //if(Input.touchCount > 0)
-        //{
-        //    if (Input.GetTouch(0).phase == TouchPhase.Ended)
-        //}
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount > 0)
         {
-            //    RaycastHit hit;
-            //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            //    EffectGO.transform.position = oldPos;
-            //    direction = oldPos - ray.GetPoint(5);
-            //    oldPos = ray.GetPoint(5);
-
-            //    if (Physics.Raycast(ray, out hit, 500, layer))
-            //    {
-            //        Transform objectHit = hit.collider.transform;
-
-            //        if (objectHit.TryGetComponent(out IHitable hitable))
-            //        {
-            //            if (hitable is Fruit)
-            //            {
-            //                Fruit fruit = hitable as Fruit;
-            //                fruit.Slice(direction, oldPos, 10f);
-            //            }
-            //            else
-            //            {
-            //                Crate crate = hitable as Crate;
-            //                crate.TakeHit();
-            //            }
-            //        }
-            //    }
-            //}
             Raycast(
                 (hitable) =>
                 {
@@ -54,7 +22,7 @@ public class Pointer : MonoBehaviour
                     }
                 });
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount> 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             Raycast(
                (hitable) =>
